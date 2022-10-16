5462d844ef9917c00793bf1a65badc1ba8defec4
Pavel Polin <pavel.polin.59@gmail.com>
2022-10-16 11:17:47 +0500

    add day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,43 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car lada = new Car();
+        lada.setModel("Vesta");
+        lada.setColor("black");
+        lada.setYear(2017);
+        System.out.println("Модель: " + lada.getModel());
+        System.out.println("Цвет: "+ lada.getColor());
+        System.out.println("Год выпуска: " + lada.getYear());
 
+    }
+}
+
+class Car{
+    private String model;
+    private String color;
+    private int year;
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,44 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+    Motorbike honda = new Motorbike("CBR650R", "red", 2022);
+    System.out.println("Модель: " + honda.getModel());
+    System.out.println("Цвет: " + honda.getColor());
+    System.out.println("Год выпуска: " + honda.getYear());
+    }
+}
 
+class Motorbike{
+    private String model;
+    private String color;
+    private int year;
+
+    public Motorbike(String model, String color, int year){
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
